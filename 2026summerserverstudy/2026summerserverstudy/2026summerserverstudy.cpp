bf75cb0edb5862ff93f4d91411ac206b957d540d
leocho12 <hjc021212@gmail.com>
2026-02-01 15:45:12 +0900

    event

--- 2026summerserverstudy/2026summerserverstudy/2026summerserverstudy.cpp
+++ 2026summerserverstudy/2026summerserverstudy/2026summerserverstudy.cpp
@@ -4,62 +4,54 @@
 #include <thread>
 #include <atomic>
 #include <mutex>// lock용 해더
+#include <windows.h>
+#include <chrono>
 
 
-//Sleep
+//이벤트
 
-class SpinLock {
-public:
-	void lock() {
-		// CAS (Compare-And-Swap)
 
-		bool expected = false;
-		bool desired = true;
-		
-		while (_locked.compare_exchange_strong(expected, desired) == false) {
-			expected = false;
-
-			//this_thread::sleep_for(std::chrono::milliseconds(100));
-			//this_thread::sleep_for(0ms);//지정한 시간까지 스레드를 일시정지
-			this_thread::yield();//다른 스레드에게 실행 기회를 양보
-		}
-	}
-
-	void unlock() {
-		_locked.store(false);
-	}
-private:
-	atomic<bool> _locked = false;//동시 락 획득을 방지하기 위해 atomic으로 선언
-};
-
-int32 sum = 0;
 mutex m;
-SpinLock spinLock;
+queue<int32> q;
+HANDLE handle;
 
-void Add() {
-	for (int32 i = 0; i < 100000; i++) {
-		//lock_guard<사용할 락의 타입>락가드의 변수명(잠글 락 객체)
-		lock_guard<SpinLock>guard(spinLock);
-		sum++;
+void Producer() {
+	while (true) {
+		unique_lock<mutex> lock(m);
+		q.push(100);
 	}
+	::SetEvent(handle);
+
+	this_thread::sleep_for(10000ms);
 }
 
-void Sub() {
-	for (int32 i = 0; i < 100000; i++) {
-		lock_guard<SpinLock>guard(spinLock);
-		sum--;
+void Consumer() {
+	while (true) {
+		::WaitForSingleObject(handle, INFINITE);//signal,nonsignal 상태에 따라 더이상 내려가지 않고 대기
+		//::ResetEvent(handle); //수동리셋이라면 여기서 리셋
+
+		unique_lock<mutex> lock(m);
+		if (q.empty() == false) {
+			int32 data = q.front();
+			q.pop();
+			cout << data << endl;
+		}
 	}
 }
 
 int main()
 {
-	thread t1(Add);
-	thread t2(Sub);
+	// 커널 오브젝트(운체의 커널이 관리하는 객체)
+	// Usage Count(몇명이 이 오브젝트를 사용하고있나)
+	// signal (파란불)/ Nonsignal (빨간불) << bool
+	// auto / manual << bool
+	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*수동리셋여부*/, FALSE/*이벤트초기상태*/,NULL/*이름*/);
+
+	thread t1(Producer);
+	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
 
-	cout << sum << endl;
-
-
+	::CloseHandle(handle);
 }
