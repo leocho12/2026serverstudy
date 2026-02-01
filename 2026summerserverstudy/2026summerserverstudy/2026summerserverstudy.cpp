3029355516ca788ddbf35d3e0df2a82eb9c7a4ed
leocho12 <hjc021212@gmail.com>
2026-02-01 13:19:50 +0900

    spinlock

--- 2026summerserverstudy/2026summerserverstudy/2026summerserverstudy.cpp
+++ 2026summerserverstudy/2026summerserverstudy/2026summerserverstudy.cpp
@@ -5,37 +5,73 @@
 #include <atomic>
 #include <mutex>// lock용 해더
 
-vector<int32> v;
-//Mutual Exclusion : 상호 배타적
-mutex m;//일종의 자물쇠
 
-//RAII 패턴 (Resource Acquisition Is Initialization)
+//스핀 락
+// 락을 획득할 때 까지 계속 반복해서 시도
+// CPU 점유율이 높음
+// 컨텍스트 스위칭이 없음
+// 빠른 작업에 적합
 
-void Push(){
-	for (int32 i = 0; i < 10000; i++) {
-		m.lock();//자물쇠 잠금
-		//m.lock(); 일반 뮤텍스는 재귀적 불가 데드락발생
-		std::unique_lock<std::mutex> uniq_lock(m, std::defer_lock);// unique_lock : 만들자 마자 잠기지 않고 잠기는 시점을 정할 수 있음
-		v.push_back(i);
+class SpinLock {
+public:
+	void lock() {
+		// CAS (Compare-And-Swap)
 
-		if (i == 5000) {
-			m.unlock();
-			break;
+		bool expected = false;
+		bool desired = true;
+		//CAS 의사코드
+		/*
+		if (_locked == expected) {
+			expected = _locked;
+			_locked = desired;
+			return true;
 		}
+		else {
+			expected = _locked;
+			return false;
+		}
+		*/
+		//이 코드를 아래와 같이 한번에 묶어서 사용할 수	있음
+		while (_locked.compare_exchange_strong(expected, desired) == false) {
+			expected = false;
+		}
+	}
 
-		m.unlock();//자물쇠 해제
-		//m.unlock();
+	void unlock() {
+		_locked.store(false);
+	}
+private:
+	atomic<bool> _locked = false;//동시 락 획득을 방지하기 위해 atomic으로 선언
+};
+
+int32 sum = 0;
+mutex m;
+SpinLock spinLock;
+
+void Add() {
+	for (int32 i = 0; i < 100000; i++) {
+		//lock_guard<사용할 락의 타입>락가드의 변수명(잠글 락 객체)
+		lock_guard<SpinLock>guard(spinLock);
+		sum++;
+	}
+}
+
+void Sub() {
+	for (int32 i = 0; i < 100000; i++) {
+		lock_guard<SpinLock>guard(spinLock);
+		sum--;
 	}
 }
 
 int main()
 {
-	std::thread t1(Push);
-	std::thread t2(Push);
+	thread t1(Add);
+	thread t2(Sub);
 
 	t1.join();
 	t2.join();
 
-	cout << v.size() << endl;
-	
+	cout << sum << endl;
+
+
 }
