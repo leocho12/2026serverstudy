3d961c48eaa31f48f47efb09797247f2f0255029
leocho12 <hjc021212@gmail.com>
2026-02-04 14:48:29 +0900

    future

--- 2026summerserverstudy/2026summerserverstudy/2026summerserverstudy.cpp
+++ 2026summerserverstudy/2026summerserverstudy/2026summerserverstudy.cpp
@@ -4,69 +4,101 @@
 #include <thread>
 #include <atomic>
 #include <mutex>// lock용 해더
-#include <windows.h>
-#include <chrono>
+#include <future>
 
+//Future
 
-//컨디션 변수
+int64 result;
 
+int64 Calculate() {
+	int64 sum = 0;
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
-
-condition_variable cv;
-// 참고) 컨디션 변수는 유저레벨 오브젝트
-
-void Producer() {
-
-	while (true) {
-	// 1. Lock을 잡고
-		unique_lock<mutex> lock(m);
-	// 2. 공유변수 값 수정
-		q.push(100);
+	for (int32 i = 0; i < 100000; i++) {
+		sum += i;
 	}
-	// 3. Lock 해제(자동)
+	result = sum;
 
-	// 4. 컨디션 변수로 다른 쓰레드에 통지
-	cv.notify_one(); // wait중인 쓰레드 하나를 깨운다.
-
-	//this_thread::sleep_for(10000ms);
+	return sum;
 }
 
-void Consumer() {
-	while (true) {
-		
-		// 1. Lock을 잡고
-		unique_lock<mutex> lock(m);
+void PromiseWorker(std::promise<string>&& promise) {
+	promise.set_value("secret message");
+}
 
-		// 2. 조건확인
-		cv.wait(lock, [](){return q.empty() == false;});
-		// -만족X -> Lock 해제 + 대기
-
-		// -만족O -> Lock 유지 + 진행
-		//while (q.empty() == false) 컨디션 변수 안에 루프가 포함되어 있음
-		{
-			int32 data = q.front();
-			q.pop();
-			cout << data << endl;
-		}
-	}
+void TaskWorker(std::packaged_task<int64(void)>&& task) {
+	task();
 }
 
 int main()
 {
-	// 커널 오브젝트(운체의 커널이 관리하는 객체)
-	// Usage Count(몇명이 이 오브젝트를 사용하고있나)
-	// signal (파란불)/ Nonsignal (빨간불) << bool
-	// auto / manual << bool
-	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*수동리셋여부*/, FALSE/*이벤트초기상태*/,NULL/*이름*/);
+	// 동기(synchronous)실행 : 호출하는 순간 실행
+	//int64 sum =Calculate();
+	//cout << sum << endl;
 
-	thread t1(Producer);
-	thread t2(Consumer);
 
-	t1.join();
-	t2.join();
+	// std::future
+	{
+		// 언젠가 완료될 작업의 결과물을 약속하는 객체
+		// 비동기(asynchronous)실행 : 호출하는 순간 바로 리턴
+		// 1) deferred(지연) : 호출하는 순간 실행하지 않고, get()호출하는 순간 실행
+		// 2) async(비동기) : 호출하는 순간 별도의 스레드에서 실행
+		// 3) deffered | async : 호출하는 순간 상황에 따라 실행
+		std::future<int64> future = std::async(std::launch::async, Calculate);
 
-	::CloseHandle(handle);
+		// 작업수행
+
+		int64 sum = future.get(); // 결과물이 이제 필요하다!
+
+		// 멤버변수를 호출하는 경우
+		{
+			class Knight {
+			public:
+				int64 GetHp() { return 100; }
+			};
+			Knight knight;
+			std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHp, knight);
+		}
+	}
+
+
+	// std::promise
+	{
+		// 미래(std::future)에 결과물을 반환할거라고 약속
+		// promise로 값을 설정하고, future로 값을 얻어옴
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
+
+		thread t(PromiseWorker, std::move(promise));
+
+		string message = future.get();
+		cout << message << endl;
+
+		t.join();
+	}
+
+
+	// std::packaged_task
+	{
+		// packaged_task로 값을 설정하고, future로 값을 얻어옴
+		std::packaged_task<int64(void)> task(Calculate);
+		// 결과물 자체를 future객체를 통해 받아올 수 있음
+		std::future<int64> future = task.get_future();
+
+		std::thread t(TaskWorker, std::move(task));
+
+		int64 sum = future.get();
+		cout << sum << endl;
+		t.join();
+	}
+
+
+	// 요약)
+	// mutex, condition_variable까지 사용하지 않고 단순한 작업ㅇ르 처리할수있는 방법들이다
+	// 일회성으로 한번만 일어나는 이밴트에 대해 효과적
+	// 1. future-async
+	// 원하는 함수를 비동기적으로 실행
+	// 2. promise
+	// promise객체로 값을 설정하고, future객체로 값을 얻어옴
+	// 3. packaged_task
+	// 특정 작업을 나중에 실행하고, future객체로 값을 얻어옴
 }
