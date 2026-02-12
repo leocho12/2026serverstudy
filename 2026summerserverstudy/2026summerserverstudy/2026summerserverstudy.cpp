15c9e770b79d73d5d65dbcb5c16fd697e1a935d1
leocho12 <hjc021212@gmail.com>
2026-02-12 18:04:09 +0900

    lockfreestack

--- 2026summerserverstudy/2026summerserverstudy/2026summerserverstudy.cpp
+++ 2026summerserverstudy/2026summerserverstudy/2026summerserverstudy.cpp
@@ -10,7 +10,7 @@
 #include "ConcurrentQueue.h"
 #include "ConcurrentStack.h"
 
-//Lock based Queue and Stack test
+//Lock free Stack
 
 LockQueue<int32> q;
 LockStack<int32> s;
@@ -18,7 +18,7 @@ LockStack<int32> s;
 void Push() {
 	while (true) {
 		int32 value = rand() % 100;
-		q.Push(value);
+		s.Push(value);
 		this_thread::sleep_for(10ms);
 	}
 }
@@ -26,7 +26,7 @@ void Push() {
 void Pop() {
 	while (true) {
 		int32 data = 0;
-		if (q.TryPop(OUT data))
+		if (s.TryPop(OUT data))
 			cout << data << endl;
 		
 	}
@@ -35,7 +35,9 @@ void Pop() {
 int main() {
 	thread t1(Push);
 	thread t2(Pop);
+	thread t3(Pop);
 	t1.join();
 	t2.join();
+	t3.join();
 
 }
--- 2026summerserverstudy/2026summerserverstudy/ConcurrentStack.h
+++ 2026summerserverstudy/2026summerserverstudy/ConcurrentStack.h
@@ -50,3 +50,116 @@ private:
 	mutex _mutex;
 	condition_variable _condVar;
 };
+
+template <typename T>
+class LockFreeStack {
+	struct Node {
+		Node(const T& value) : data(value), next(nullptr){
+
+		}
+		T data;
+		Node* next;
+	};
+
+public:
+	void Push(const T& value) {
+		Node* node = new Node(value);
+		node->next = _head;
+
+		/*if (_head == node->next) {
+			_head = node;
+			return;
+		}
+		else {
+			node->next = _head;
+			return false;
+		}*/
+		//이걸 원자적으로 한번에 실행하는게 compare_exchange_weak
+		while(_head.compare_exchange_weak(node->next, node)==false) {
+
+		}
+	}
+
+	bool TryPop(T& value) {
+		++_popCount;
+		Node* oldHead = _head;
+
+		while(oldHead && _head.compare_exchange_weak(oldHead, oldHead->next)==false) {
+			
+		}
+		if (oldHead == nullptr) {
+			--_popCount;
+			return false;
+		}
+		value = oldHead->data;
+		
+		TryDelete(oldHead);
+		
+		return true;
+	}
+
+	//1.데이터 분리
+	//2. count체크
+	//3. 혼자면 삭제
+	//4. 여러명이면 pendingList에 추가
+	void TryDelete(Node* oldHead) {
+		//나 이외에	pop을 실행중인 쓰레드가 없으면
+		if (_popCount == 1) {
+			//나 혼자만 실행중
+
+			//pendingList에 남아있는 노드들 삭제
+			Node* node = _pendingList.exchange(nullptr);//값 빼오기
+			if (--_popCount == 0) {
+				//끼어든	쓰레드 없음->삭제
+				//다른 쓰레드가 끼어들어도 데이터는 분리되어있음
+				DeleteNodes(node);
+			}
+			else if(node){
+				//다른 쓰레드가 끼어들었음->pendingList에 다시 연결
+				ChainPendingNodeList(node);
+			}
+
+			//내 데이터 삭제
+			delete oldHead;
+		}
+		else {
+			//나 이외에 pop 실행중인 쓰레드가 있음->pendingList에 내 데이터 연결
+			ChainPendingNodeList(oldHead);
+			--_popCount;
+		}
+	}
+
+	void ChainPendingNodeList(Node* first, Node* last) {
+		last->next = _pendingList;
+
+		while(_pendingList.compare_exchange_weak(last->next, first) == false) {
+
+		}
+	}
+
+	void ChainPendingNodeList(Node* node) {
+		Node* last = node;
+		while(last->next)
+			last = last->next;
+
+		ChainPendingNodeList(node, last);
+	}
+
+	void ChainPendingNode(Node* node) {
+		ChainPendingNodeList(node, node);
+	}
+
+	static void DeleteNodes(Node* node) {
+		while (node) {
+			Node* next = node->next;
+			delete node;
+			node = next;
+		}
+	}
+
+private:
+	atomic<Node*> _head;
+
+	atomic<uint32>_popCount = 0;// pop을 실행중인 쓰레드 개수
+	atomic<Node*> _pendingList;// 삭제 되어야 할 노드들(첫번째 노드만 기억)
+};
